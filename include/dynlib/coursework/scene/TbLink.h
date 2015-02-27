a8dc6cc55392f1827f8803a430f57ca0d48c54d0
BREAK350 <break350@gmail.com>
2015-02-27 14:17:23 +0200

    add empty class TbLink

---
+++ include/dynlib/coursework/scene/TbLink.h
@@ -0,0 +1,52 @@
+//---------------------------------------------------------------------------
+#ifndef TbLinkH
+#define TbLinkH
+//---------------------------------------------------------------------------
+// classes
+namespace dynlib
+{
+	namespace coursework
+	{
+		namespace scene
+		{
+			class TbLink;
+		}
+	}
+}
+//---------------------------------------------------------------------------
+// includes
+
+//---------------------------------------------------------------------------
+namespace dynlib
+{
+	namespace coursework
+	{
+		namespace scene
+		{
+			class TbLink
+			{
+				// fields
+				private:
+				protected:
+				public:
+
+				// static fields
+				private:
+				protected:
+				public:
+
+				// methods
+				private:
+				protected:
+				public:
+
+				// static methods
+				private:
+				protected:
+				public:
+            };
+		}
+	}
+}
+//---------------------------------------------------------------------------
+#endif
