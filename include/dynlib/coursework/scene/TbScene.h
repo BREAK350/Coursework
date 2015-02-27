66062fe8208fe820d8c14ea2ce2a829c5d586fa2
BREAK350 <break350@gmail.com>
2015-02-27 13:54:14 +0200

    add class TbScene

---
+++ include/dynlib/coursework/scene/TbScene.h
@@ -0,0 +1,52 @@
+//---------------------------------------------------------------------------
+#ifndef TbSceneH
+#define TbSceneH
+//---------------------------------------------------------------------------
+// classes
+namespace dynlib
+{
+	namespace coursework
+	{
+		namespace scene
+		{
+			class TbScene;
+		}
+	}
+}
+//---------------------------------------------------------------------------
+// includes
+
+//---------------------------------------------------------------------------
+namespace dynlib
+{
+	namespace coursework
+	{
+		namespace scene
+		{
+			class TbScene
+			{
+				// fields
+				private:
+				protected:
+				public:
+
+				// static fields
+				private:
+				protected:
+				public:
+
+				// methods
+				private:
+				protected:
+				public:
+
+				// static methods
+				private:
+				protected:
+				public:
+            };
+		}
+	}
+}
+//---------------------------------------------------------------------------
+#endif
