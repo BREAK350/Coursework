bb401a0e9c5b47c1cfda8f00afce67982f294219
BREAK350 <break350@gmail.com>
2015-02-27 14:06:12 +0200

    add empty class TbGroupState

---
+++ include/dynlib/coursework/scene/TbGroupState.h
@@ -0,0 +1,52 @@
+//---------------------------------------------------------------------------
+#ifndef TbGroupStateH
+#define TbGroupStateH
+//---------------------------------------------------------------------------
+// classes
+namespace dynlib
+{
+	namespace coursework
+	{
+		namespace scene
+		{
+			class TbGroupState;
+		}
+	}
+}
+//---------------------------------------------------------------------------
+// includes
+
+//---------------------------------------------------------------------------
+namespace dynlib
+{
+	namespace coursework
+	{
+		namespace scene
+		{
+			class TbGroupState
+			{
+				// fields
+				private:
+				protected:
+				public:
+
+				// static fields
+				private:
+				protected:
+				public:
+
+				// methods
+				private:
+				protected:
+				public:
+
+				// static methods
+				private:
+				protected:
+				public:
+            };
+		}
+	}
+}
+//---------------------------------------------------------------------------
+#endif
