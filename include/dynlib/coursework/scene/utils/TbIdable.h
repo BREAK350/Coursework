db4c8a94b3a074ec36706713aea4d09c54298878
BREAK350 <break350@gmail.com>
2015-02-27 15:22:13 +0200

    add interface TbIdable

---
+++ include/dynlib/coursework/scene/utils/TbIdable.h
@@ -0,0 +1,23 @@
+//---------------------------------------------------------------------------
+#ifndef TbIdableH
+#define TbIdableH
+//---------------------------------------------------------------------------
+namespace dynlib
+{
+	namespace coursework
+	{
+		namespace scene
+		{
+			namespace utils
+			{
+				class TbIdable
+				{
+					public:
+						virtual int getId() = 0;
+				};
+			}
+		}
+	}
+}
+//---------------------------------------------------------------------------
+#endif
