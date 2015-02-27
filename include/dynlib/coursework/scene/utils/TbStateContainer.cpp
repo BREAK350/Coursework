68f95288d6fe63e9b8ff83c3de3724da4254e188
BREAK350 <break350@gmail.com>
2015-02-27 15:08:01 +0200

    add empty class TbStateContainer

---
+++ include/dynlib/coursework/scene/utils/TbStateContainer.cpp
@@ -0,0 +1,19 @@
+//---------------------------------------------------------------------------
+#pragma hdrstop
+#include "TbStateContainer.h"
+#pragma package(smart_init)
+//---------------------------------------------------------------------------
+namespace dynlib
+{
+	namespace coursework
+	{
+		namespace scene
+		{
+            namespace utils
+			{
+
+			}
+		}
+	}
+}
+//---------------------------------------------------------------------------
---
+++ include/dynlib/coursework/scene/utils/TbStateContainer.h
@@ -0,0 +1,59 @@
+//---------------------------------------------------------------------------
+#ifndef TbStateContainerH
+#define TbStateContainerH
+//---------------------------------------------------------------------------
+// classes
+namespace dynlib
+{
+	namespace coursework
+	{
+		namespace scene
+		{
+			namespace utils
+			{
+				class TbStateContainer;
+            }
+		}
+	}
+}
+//---------------------------------------------------------------------------
+// includes
+
+//---------------------------------------------------------------------------
+namespace dynlib
+{
+	namespace coursework
+	{
+		namespace scene
+		{
+			namespace utils
+			{
+				class TbStateContainer
+				{
+					// fields
+					private:
+					protected:
+					public:
+
+					// static fields
+					private:
+					protected:
+					public:
+
+					// methods
+					private:
+					protected:
+					public:
+
+					// static methods
+					private:
+					protected:
+					public:
+				};
+
+			}
+		}
+	}
+}
+//---------------------------------------------------------------------------
+#endif
