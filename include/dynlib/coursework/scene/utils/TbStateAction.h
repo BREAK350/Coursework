6702a90e29acb49e3dd7270af0d19e33d95b9819
BREAK350 <break350@gmail.com>
2015-02-27 15:59:35 +0200

    add interface TbStateAction

---
+++ include/dynlib/coursework/scene/utils/TbStateAction.h
@@ -0,0 +1,26 @@
+//---------------------------------------------------------------------------
+#ifndef TbStateActionH
+#define TbStateActionH
+//---------------------------------------------------------------------------
+// includes
+#include "TbState.h"
+//---------------------------------------------------------------------------
+namespace dynlib
+{
+	namespace coursework
+	{
+		namespace scene
+		{
+			namespace utils
+			{
+				class TbStateAction
+				{
+					public:
+						virtual void action(TbState *state) = 0;
+				};
+			}
+		}
+	}
+}
+//---------------------------------------------------------------------------
+#endif
